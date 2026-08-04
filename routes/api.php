8525af7e54ffc0f01ac358fd6074205d7491e173
Yehia Kamel <yehiak257@gmail.com>
2026-08-04 04:23:35 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -8,25 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::prefix('meals')->controller(MealsController::class)->group(function(){
-    Route::get('/','index');
-    Route::get('/{meal}','show');
-});
-
-Route::prefix('meals')->middleware('auth:sanctum')->controller(MealsController::class)->group(function(){
-    Route::post('/','store');
-    Route::put('/{meal}','update');
-    Route::delete('/{meal}','destroy');
-});
-
-
-
-Route::controller(AuthController::class)->group(function(){
-    Route::post('register','register');
-    Route::post('login','login');
-});
-Route::middleware('auth:sanctum')->controller(AuthController::class)->group(function(){
-    Route::get('logout','logout');
-});
-
